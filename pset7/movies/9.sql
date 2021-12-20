8ba48e1e0c44d240415db03481cece6d46fcde20
aded65666968 <92978761+aded65666968@users.noreply.github.com>
2021-12-20 08:24:51 +0200

    Create 9.sql

---
+++ pset7/movies/9.sql
@@ -0,0 +1,8 @@
+-- write a SQL query to list the names of all people who starred in a movie released in 2004
+-- Ordered by birth year.
+-- Eliminate duplicates 
+-- Caefully count
+
+SELECT name 
+FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON movies.id = stars.movie_id
+WHERE year = 2004 ORDER BY birth;
